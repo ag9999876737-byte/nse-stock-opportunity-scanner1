a3f4eb335940a9414384303deeab9f499c84d632
ag9999876737-byte <ag9999876737@gmail.com>
2026-04-29 20:29:00 +0530

    Update news.py

--- services/news.py
+++ services/news.py
@@ -1,11 +1,11 @@
 import feedparser
 
 def get_news(query):
-    url = f"https://news.google.com/rss/search?q={query}+stock+india&hl=en-IN&gl=IN&ceid=IN:en"
-    feed = feedparser.parse(url)
+    try:
+        url = f"https://news.google.com/rss/search?q={query}+stock+india"
+        feed = feedparser.parse(url)
 
-    news = []
-    for entry in feed.entries[:5]:
-        news.append(entry.title)
+        return [entry.title for entry in feed.entries[:5]]
 
-    return news
+    except Exception:
+        return []
